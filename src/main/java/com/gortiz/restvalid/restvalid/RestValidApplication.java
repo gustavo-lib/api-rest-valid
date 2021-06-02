7f4aaa849ba051d9b4f1fc49d6a648167c2a7224
gortiz <ggortiz09@gmail.com>
2021-06-02 19:13:05 -0300

    first commit gabo ;)

---
+++ src/main/java/com/gortiz/restvalid/restvalid/RestValidApplication.java
@@ -0,0 +1,13 @@
+package com.gortiz.restvalid.restvalid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestValidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestValidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gortiz/restvalid/restvalid/RestValidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gortiz.restvalid.restvalid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestValidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
